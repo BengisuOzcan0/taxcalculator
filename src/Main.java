eafd9113c321b163e6e183ca721bc5cbe2470759
BengisuOzcan0 <bozcan2@stu.vistula.edu.pl>
2023-10-19 22:01:34 +0200

    msg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.math.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BigDecimal Price_before_VAT = new BigDecimal("9.99");
+
+        System.out.println("Price without VAT:" + Price_before_VAT);
+
+        BigDecimal vatRate =new BigDecimal("0.23");
+
+
+        BigDecimal Price_WithVAT = Price_before_VAT.multiply(BigDecimal.ONE.add(vatRate));
+        System.out.println("Price with VAT:" + Price_WithVAT);
+
+
+        BigDecimal totalExcludingVAT = Price_before_VAT.multiply(new BigDecimal("10000"));
+
+
+        System.out.println("Value with VAT : " + Price_WithVAT);
+        System.out.println("Value without VAT : " + totalExcludingVAT);
+
+
+        int comparison = Price_WithVAT.compareTo(totalExcludingVAT);
+        if (comparison > 0) {
+            System.out.println("Price with VAT is greater than Value without VAT.");
+        } else if (comparison < 0) {
+            System.out.println("Value without VAT is greater than Gross Value.");
+        } else {
+            System.out.println("Price with VAT is equal to Value with VAT");
+        }
+
     }
 }
